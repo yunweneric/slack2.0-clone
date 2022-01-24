64e8dfb72bf001164660d079fd46fd7eda4c0741
Erickeyz <yunweneric@gmail.com>
2022-01-24 13:00:58 +0100

    project init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBKxazbu1V51NKpRTc7mxv3FfK-48OI-s",
+    authDomain: "slack-clone2p0.firebaseapp.com",
+    projectId: "slack-clone2p0",
+    storageBucket: "slack-clone2p0.appspot.com",
+    messagingSenderId: "802133048343",
+    appId: "1:802133048343:web:7713ded0be4ad50ef2f052"
+};
