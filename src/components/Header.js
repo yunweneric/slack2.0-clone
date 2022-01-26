efbe49cbb73e2d1a4d4e365973a3a4af4107a5fc
yunweneric <yunweneric@gmail.com>
2022-01-26 05:46:52 +0100

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+
+      <HeaderSearch>
+        <SearchIcon />
+        <input type="Search for @yunwen" />
+      </HeaderSearch>
+
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--slack-color);
+  color: white;
+  padding: 10px;
+`;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  display: flex;
+  opacity: 1;
+  color: grey;
+  border-radius: 6px;
+  padding: 0 50px;
+  text-align: center;
+  border: 1px solid grey;
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+`;
+const HeaderAvatar = styled(Avatar)``;
